294d59f1316b6ee9fa3f6c0c55314c0d087075b4
jayniparekh <jayniaparekh@gmail.com>
2026-02-13 22:04:29 +0530

    Setup backend folder structure and MongoDB connection

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+async function db() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log(`Mongo DB connected!`);
+    } catch (error) {
+        console.log("Mongo DB connection error: ", error);
+        process.exit(1);
+    }
+}
+
+export default db();
