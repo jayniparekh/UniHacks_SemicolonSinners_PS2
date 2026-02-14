cf40d7b3cde0560817dd59b22c186fd5e4c2baa1
jayniparekh <jayniaparekh@gmail.com>
2026-02-14 11:02:05 +0530

    funct:updateProfileImage

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from '../models/User.js';
+import { uploadToCloudinary } from '../utils/cloudinary.js';
 
 export const updateUser = async (req, res) => {
     try{
@@ -19,5 +20,26 @@ export const updateUser = async (req, res) => {
     }
     catch(error){
         res.status(500).json({ message: "Error updating user", error: error.message });
+    }
+};
+
+export const updateProfileImage = async (req, res) => {
+    try {
+        // 1. Get the file path (usually from middleware like Multer)
+        const filePath = req.file.path;
+
+        // 2. Upload to Cloudinary
+        const result = await uploadToCloudinary(filePath);
+
+        // 3. Save the secure_url to your User Schema
+        const updatedUser = await User.findByIdAndUpdate(
+            req.user.id,
+            { "reviewerProfile.profileImage": result.secure_url },
+            { new: true }
+        );
+
+        res.json({ message: "Image uploaded!", url: result.secure_url });
+    } catch (error) {
+        res.status(500).json({ message: "Upload failed", error });
     }
 };
