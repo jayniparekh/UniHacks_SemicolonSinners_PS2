5b54c254057b5fa044d4405e8e191a7b47f9d7f6
jayniparekh <jayniaparekh@gmail.com>
2026-02-14 11:06:52 +0530

    feat:added upload-photo routes

--- server/routes/profileRoutes.js
+++ server/routes/profileRoutes.js
@@ -4,11 +4,14 @@ import {
   updateMyProfile,
   createProfileSubmission
 } from "../controllers/profileController.js";
+import upload from '../middleware/multer.js';
+import { updateProfileImage } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get("/me", getMyProfile);
 router.put("/me", updateMyProfile);
 router.post("/", createProfileSubmission);
+router.post('/upload-photo', upload.single('image'), updateProfileImage);
 
 export default router;
